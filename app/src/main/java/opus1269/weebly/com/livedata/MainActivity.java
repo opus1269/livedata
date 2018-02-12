4133e597bdd3fa221564e6d91a0dcce60b9cd76e
opus1269 <igreilly1269@gmail.com>
2018-02-11 22:26:35 -0600

    inspection bug

--- app/src/main/java/opus1269/weebly/com/livedata/MainActivity.java
+++ app/src/main/java/opus1269/weebly/com/livedata/MainActivity.java
@@ -25,7 +25,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -52,17 +51,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(view ->
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                        .setAction("Action", null).show());
     }
 
     private void inspectionProblem(String text) {
-        Log.d("TAG", text);
+        if (text != null) {
+            Log.d("TAG", "text is: " + text);
+        } else {
+            Log.d("TAG", "text is: null");
+        }
     }
 
     @Override
