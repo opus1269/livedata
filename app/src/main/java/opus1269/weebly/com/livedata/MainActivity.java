9a12f41d5700328a82b9e89cc0b7059a4a644497
opus1269 <igreilly1269@gmail.com>
2018-02-11 22:18:07 -0600

    inspection bug

--- app/src/main/java/opus1269/weebly/com/livedata/MainActivity.java
+++ app/src/main/java/opus1269/weebly/com/livedata/MainActivity.java
@@ -24,6 +24,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -43,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         binding.setVm(viewModel);
         binding.executePendingBindings();
 
+        // Inspection says inspectionProblem param is always null but not true
+        viewModel.text.observe(this, this::inspectionProblem);
+        inspectionProblem(null);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -54,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void inspectionProblem(String text) {
+        Log.d("TAG", text);
     }
 
     @Override
